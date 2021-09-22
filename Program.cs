7ca3d247e8660cd113f2ab9f70c2d5584322bc02
Shruti <shrutisablaniya93@gmail.com>
2021-09-22 12:54:37 +0530

    Main Class

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book\n");
+
+
+
+
+            //UC1_CreateContact.CreateContact();
+            //UC2_AddNewContact.AddNewContacts();
+            //UC3_UpdateExistingContact.UpdateContact();
+            //UC4_DeleteContact.ContactDelete();
+            //UC5_AddMultipleContacts.AddMultipleContacts();
+            UC6_MultipleAddressBook.multipleAddressBook();
+            Console.ReadKey();
+        }
+    }
+}
