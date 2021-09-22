6e09818ea1d68b37645d64bfaa5ad65ae0a1abf5
Shruti <shrutisablaniya93@gmail.com>
2021-09-22 12:57:50 +0530

    Use Case 2

---
+++ UC2_AddNewContact.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+
+    class UC2_AddNewContact
+    {
+        static String firstName;
+        static String lastName;
+        static long phoneNumber;
+        static String email;
+        static String address;
+        static String city;
+        static String state;
+        static int zipCode;
+        public static List<String> addressBook = new List<String>();
+
+        // method for add new contact
+        public static void AddNewContacts()
+        {
+            
+            CreateContact(addressBook);
+        }
+
+
+        // method for creating contact in address book
+        public static void CreateContact(List<string> addressBook)
+        {
+            int count = 0;
+            string contact;
+            char ch;
+            do
+            {
+                Console.WriteLine("\nPlease Enter Contact Details: \n");
+                ContactDeatils();
+                Console.WriteLine("\nContact Created.\n");
+
+                contact = firstName + " " + lastName + " " + phoneNumber.ToString() + " " + email + " " + address + " " + city + " " + state + " " + zipCode.ToString();
+                addressBook.Add(contact);
+                count++;
+
+                Console.WriteLine("Do you want to continue: y/n");
+                ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("\n---------------------------------------------------------------------------------------------------------");
+            }
+            while (ch == 'y');
+
+
+            Console.WriteLine("\n{0} contacts\n", count);
+
+            for (int i = 0; i < addressBook.Count; i++)
+            {
+                Console.WriteLine("{0}.  {1}", i+1, addressBook[i]);
+            }
+            toContinue();
+        }
+
+        // method for taking user input from console for creating or updating a contact
+        public static void ContactDeatils()
+        {
+
+            Console.Write("First Name : ");
+            firstName = Console.ReadLine();
+            Console.Write("Last Name : ");
+            lastName = Console.ReadLine();
+            Console.Write("Phone Number : ");
+            phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Email : ");
+            email = Console.ReadLine(); ;
+            Console.Write("Address : ");
+            address = Console.ReadLine();
+            Console.Write("City : ");
+            city = Console.ReadLine();
+            Console.Write("State : ");
+            state = Console.ReadLine();
+            Console.Write("Zip : ");
+            zipCode = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+
+        public static void toContinue()
+        {
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------------------");
+            char ch;
+            Console.WriteLine("Do you want to continue: y/n");
+            ch = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("\n\n");
+            if (ch == 'y')
+            {
+                AddNewContacts();
+            }
+            else
+            {
+                Environment.Exit(-1);
+            }
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------------------");
+        }
+
+
+
+    }
+}
