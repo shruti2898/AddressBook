e1dfbe5ec1341f8b3c440741b018f6a88b9f3320
Shruti <shrutisablaniya93@gmail.com>
2021-09-22 12:56:25 +0530

    Use Case 1

---
+++ UC1_CreateContact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class UC1_CreateContact
+    {   
+        // method for creating single contact and displaying contact
+        public static void CreateContact()
+        {
+            string contact;   
+            Console.WriteLine("Please Enter Contact Details\n");
+            Console.Write("First Name : ");
+            String firstName = Console.ReadLine();
+            Console.Write("Last Name : ");
+            String lastName = Console.ReadLine();
+            Console.Write("Phone Number : ");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Email : ");
+            String email = Console.ReadLine(); ;
+            Console.Write("Address : ");
+            String address = Console.ReadLine();
+            Console.Write("City : ");
+            String city = Console.ReadLine();
+            Console.Write("State : ");
+            String state = Console.ReadLine();
+            Console.Write("Zip : ");
+            int zipCode = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("\nContact Created.\n");
+            
+            contact = firstName + " " + lastName + " " + phoneNumber.ToString() + " " + email + " " + address + " " + city + " " + state + " " + zipCode.ToString();
+
+            Console.WriteLine(contact);
+        }
+    }
+}
+
